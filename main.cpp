83ed3e9d417452c4c28cb04ea61b17f271042432
Rainy <doge20162016@gmail.com>
2026-03-10 19:20:25 +0700

    Полное переделывание
    Прошлая версия не проходила вложенности

--- main.cpp
+++ main.cpp
@@ -1,198 +1,67 @@
 #include <QCoreApplication>
-#include <QTextStream>
-#include <QDirIterator>
-//#include <QFileDialog>
-#include <QDateTime>
+#include <QDir>
 #include <QDebug>
-#include <QFileInfo>
+#include <QTextStream>
+#include <windows.h>
+void listContents(const QString &path, int indent = 0) {
+    QDir dir(path);
 
-#include <iostream>
+    // Проверяем, существует ли папка
+    if (!dir.exists()) {
+        qDebug() << "No Folder:" << path;
+        return;
+    }
 
-QTextStream cin(stdin);
-QTextStream cout(stdout);
+    // Получаем список всех файлов и папок (включая скрытые)
+    QFileInfoList entries = dir.entryInfoList(QDir::NoDotAndDotDot | QDir::AllEntries);
 
-void print_attribs(QFileInfo& info, QTextStream& stream) {
-    if (info.isReadable())
-        cout << "R";
-    if (info.isWritable())
-        cout << "W";
-    if (info.isHidden())
-        cout << "H";
-    if (info.isExecutable())
-        cout << "E";
-}
+    // Отступ для визуального отображения вложенности
+    QString indentStr(indent * 2, ' ');
 
-QString fileSize(qint64 nSize) {
-    qint64 i = 0;
-    for (; nSize > 1023; nSize /= 1024, ++i) { }
-    return QString().setNum(nSize) + "BKMGT"[i];
-}
+    for (const QFileInfo &entry : entries) {
+        if (entry.isDir()) {
+            // Выводим папку
+            qDebug().noquote() << indentStr + "[Folder]" + entry.fileName();
+                                                                   // Рекурсивно обходим содержимое папки
+                                                                   listContents(entry.absoluteFilePath(), indent + 1);
+        } else {
+            // Выводим файл с информацией о размере
+            QString sizeStr;
+            qint64 size = entry.size();
 
-void folderTraverse(QString& path) {
-    QDirIterator itDirs(path, QDir::Dirs);
-    while (itDirs.hasNext()) {
-        itDirs.next();
+            if (size < 1024) {
+                sizeStr = QString::number(size) + " B";
+            } else if (size < 1024 * 1024) {
+                sizeStr = QString::number(size / 1024.0, 'f', 2) + " KB";
+            } else if (size < 1024 * 1024 * 1024) {
+                sizeStr = QString::number(size / (1024.0 * 1024.0), 'f', 2) + " MB";
+            } else {
+                sizeStr = QString::number(size / (1024.0 * 1024.0 * 1024.0), 'f', 2) + " GB";
+            }
 
-        if (itDirs.fileName() == ".") continue;
-
-        QFileInfo info = itDirs.fileInfo();
-        cout << "D | " << info.fileName() << " | " << fileSize(info.size())
-             << " | " << info.lastModified().toString() << " | ";
-        print_attribs(info, cout);
-        cout << Qt::endl;
+            qDebug().noquote() << indentStr + "[File]" + entry.fileName() + " (" + sizeStr + ")";
+        }
     }
 }
 
-QString getValidDir() {
-    QString path;
-    cout << "Enter path for folder to encrypt: ";
-    cout.flush();
-    path = cin.readLine();
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
 
-    QDir folder(path);
-    bool valid = false;
+    QString folderPath;
+    QTextStream input(stdin);
+    QTextStream output(stdout);
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    output << "Enter path to folder: ";
+    output.flush();
+    // H:\Documents\Secure_tools_AndreevaVV\TRUE_REPO
+    folderPath = input.readLine().trimmed();
 
-    while (!folder.exists()) {
-        cout << "Path is incorrect or there is no such path :(" << Qt::endl;
-        cout << "Try again: ";
-        cout.flush();
-        path = cin.readLine();
-        folder = QDir(path);
-    }
+    output << "\nContent of  " << folderPath << ":\n";
+            output << "====================================\n";
+    output.flush();
 
-    cout << "Path found :)" << Qt::endl;
-
-    return path;
-}
-
-int main()
-{
-    QString path = getValidDir();
-    folderTraverse(path);
-
-    return 0;
-    // /Users/liza/Desktop/ТехнЧтение/
-    //return a.exec();
-}
-/*
-void print_attribs(QFileInfo& info, QTextStream& stream) {
-    if (info.isReadable())
-        stream << "R";
-    if (info.isWritable())
-        stream << "W";
-    if (info.isHidden())
-        stream << "H";
-    if (info.isExecutable())
-        stream << "E";
-}
-
-
-QString fileSize(qint64 nSize) {
-    qint64 i = 0;
-    for (; nSize > 1023; nSize /= 1024, ++i) { }
-    return QString().setNum(nSize) + "BKMGT"[i];
-}
-
-void print_all(QString path, QTextStream& stream) {
-    stream<<path;
-    QDirIterator itDirs(path, QDir::Dirs);
-    while (itDirs.hasNext()) {
-        itDirs.next();
-        if (itDirs.fileName() == ".")
-            continue;
-        QFileInfo info = itDirs.fileInfo();
-        stream << "D | " << info.fileName() << " | " << fileSize(info.size()) << " | "
-               << info.lastModified().toString() << " | ";
-        print_attribs(info, stream);
-        stream << "\n";
-    }
-    QDirIterator itFiles(path, QDir::Files);
-    while (itFiles.hasNext()) {
-        itFiles.next();
-        QFileInfo info = itFiles.fileInfo();
-        stream << "F | " << info.fileName() << " | " << fileSize(info.size()) << " | "
-               << info.lastModified().toString() << " | ";
-        print_attribs(info, stream);
-        stream << "\n";
-    }
-}
-
-*/
-
-/*
-void recursiveFolderTraversal(const QString &path, int depth = 0)
-{
-    QDirIterator it(path, QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot,
-                    QDirIterator::Subdirectories);
-
-    while (it.hasNext()) {
-        it.next();
-        QString filePath = it.filePath();
-        QFileInfo fileInfo = it.fileInfo();
-
-        // Вывод информации о файле с отступами для наглядности структуры
-        QString indent = QString(depth * 2, ' ');
-        qDebug().noquote() << indent << "Файл:" << fileInfo.fileName()
-                           << "Размер:" << fileInfo.size() << "байт";
-    }
-}
-
-
-#include <clocale>
-#include <Windows.h>
-
-int main(int argc, char *argv[])
-{
-
-    setlocale(LC_ALL, "PL_pl.UTF-8");
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-    QCoreApplication a(argc, argv);
-
-    QString startPath = QDir(""//QDir::currentPath(); // Текущая директория
-    qDebug() << "Начало обхода папки:" << startPath;
-    qDebug() << "----------------------------------------";
-
-    recursiveFolderTraversal(startPath);
+    listContents(folderPath);
 
     return 0;
 }
-*/
-
-
-//int main(int argc, char *argv[])
-//{
-    /*
-    char * str = new char;
-    std::cin>>str;
-    std::cout<<"\nEntered: "<<str<<'.'<<'\n';
-    QTextStream stream;
-
-    QString qpath = "H:/Documents/Secure_tools_AndreevaVV/papka";
-    QDir("H:/Documents/Secure_tools_AndreevaVV/papka");
-    print_all(str, stream);
-    return 0;
-    */
-
-
-
-    /*
-
-    QString dir_path;
-    char *dirr = new char;
-    QTextStream Qst;
-    Qst>>dir_path;
-    Qst<<'\n'<<dir_path;
-    //std::cin.getline(dirr, 1);
-    //std::cout<<dirr<<'\n';
-    // H:\Documents\Secure_tools_AndreevaVV\papka
-    // H:/Documents/Secure_tools_AndreevaVV/papka
-    print_all(dirr, Qst);
-
-    //QCoreApplication a(argc, argv);
-    std::cout<<"Hello from Qt and GitKraken\n";
-    std::cout<<"GitCloned\n";
-    return 0;
-    //return a.exec();
-*/
-//}
