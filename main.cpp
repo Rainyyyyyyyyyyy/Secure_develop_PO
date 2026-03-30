5665748d2332c7e412cec420d8b72971e0009d06
Rainy_ Morning <doge20162016@gmail.com>
2026-03-30 11:57:02 +0700

    Уточнение вывода для случаев файла\папки и ярлыка на него\неё
    - и добавление .gitignore

--- main.cpp
+++ main.cpp
@@ -34,6 +34,21 @@ int main(int argc, char *argv[]) {
     // E:\Z_vsyakoe_dla_echeby\4k2sem\SEcure_Develop_PO(Andreeva)\laba1\Qt\try3_gitclone\Libraries\cryptopp(defeat)
     // E:\Z_vsyakoe_dla_echeby\4k2sem\SEcure_Develop_PO(Andreeva)\laba1\Qt\try7_gitclone\papki
 
+
+    // QString folderPath = "E:\Z_vsyakoe_dla_echeby\4k2sem\SEcure_Develop_PO(Andreeva)\laba1_test_files"
+    // output:
+    /*
+    [Folder]FolderTraveler_tests
+        [Folder]papka1
+            [File]file1.txt (0 B)
+            [File]file2.txt (0 B)
+            [File]file3.txt (0 B)
+            [Folder]papka4
+                [File]file1.txt (17 B)
+            [Folder]papka2
+                [File]file2.txt.yarl.lnk (0 B)
+            [Folder]papka3
+    */
     QString folderPath;
     folderPath = input.readLine().trimmed();
 
