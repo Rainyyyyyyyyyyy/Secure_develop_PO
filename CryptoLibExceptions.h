8de534fd58bce6886cadc11c19b95cab3d5b1877
Rainy_ Morning <doge20162016@gmail.com>
2026-04-01 16:25:25 +0700

    Дополнение класса Exceptions
    - Исключение: файл слишком мал для наличия сигнатуры (соли)
    - Исключение: ошибка записи дешифрованных данных в файл
    - Исключение: ошибки записи последнего блока дешифрованных данных в файл
    === Исключения при работе функций OpenSSL ===
    - Исключение: Ошибка функции  EVP_DecryptInit_ex()
    - Исключение: Ошибка функции EVP_DecryptUpdate()
    - Исключение: EVP_DecryptFinal_ex()

--- CryptoLibExceptions.h
+++ CryptoLibExceptions.h
@@ -11,16 +11,25 @@
 #define EXCEPTION_UNABLE_TO_OPEN_FILE 1002
 #define EXCEPTION_UNABLE_TO_CREATE_FILE 1003
 #define EXCEPTION_UNABLE_TO_WRITE_ENCRYPTEDTEXT_TO_FILE 1004
+#define EXCEPTION_UNABLE_TO_WRITE_FINAL_ENCRYPTED_DATA_TO_FILE 1005
+#define EXCEPTION_UNABLE_TO_WRITE_DECRYPTEDTEXT_TO_FILE 1006
+#define EXCEPTION_UNABLE_TO_READ_SALT_FROM_FILE 1007
+
 #define EXCEPTION_OPENSSL_PKCS5_PBKDF2_HMAC 2001
-#define EXCEPTION_OPENSSL_UNABLE_TO_WRITE_SALT 2002
-#define EXCEPTION_OPENSSL_EVP_CIPHER_CTX_new 2003 // ошибка в создании контекста шифрования (openssl)
-#define EXCEPTION_OPENSSL_EVP_EncryptInit_ex 2004 // ошибка в инициализации (openssl)
-#define EXCEPTION_OPENSSL_EVP_EncryptUpdate 2005 // ошибка в шифровании блока (openssl)
-#define EXCEPTION_OPENSSL_EVP_EncryptFinal_ex 2006 // ошибка завершения шифрования (последний блок) (openssl)
+#define EXCEPTION_OPENSSL_RAND_bytes 2002   // ошибка RAND_bytes() (генерация соли) openssl)
+#define EXCEPTION_OPENSSL_UNABLE_TO_WRITE_SALT 2003
+#define EXCEPTION_OPENSSL_EVP_CIPHER_CTX_new 2004 // ошибка в создании контекста шифрования (openssl)
+#define EXCEPTION_OPENSSL_EVP_EncryptInit_ex 2005 // ошибка в инициализации (openssl)
+#define EXCEPTION_OPENSSL_EVP_EncryptUpdate 2006 // ошибка в шифровании блока (openssl)
+#define EXCEPTION_OPENSSL_EVP_EncryptFinal_ex 2007 // ошибка завершения шифрования (последний блок) (openssl)
+#define EXCEPTION_OPENSSL_EVP_DecryptInit_ex 2008 // // ошибка в инициализации (openssl)
+#define EXCEPTION_OPENSSL_EVP_DecryptUpdate 2009 // ошибка в дешифровании блока (openssl)
+#define EXCEPTION_OPENSSL_EVP_DecryptFinal_ex 2010 // ошибка завершения дешифрования (последний блок) (openssl)
+
+#define EXCEPTION_INVALID_FILE_TO_DECRYPT 3001 // файл слишком мал, для наличия сигнатуры для дешифрования
 
 
-
-    // QString.toUtf8() -> QByteArray \
+// QString.toUtf8() -> QByteArray \
     // QByteArray.constData -> cosnt char*
 
 
@@ -74,7 +83,7 @@ public:
     }
 };
 
-/* Исключение: ошибка при записи в файл */
+/* Исключение: ошибка при записи в файл зашифрованных данных  */
 class ExceptionUnableToWriteEncryptedTextToFile : public Exceptions {
 public:
     ExceptionUnableToWriteEncryptedTextToFile() : Exceptions(EXCEPTION_UNABLE_TO_WRITE_ENCRYPTEDTEXT_TO_FILE) {}
@@ -84,11 +93,43 @@ public:
     }
 };
 
+/* Исключение: ошибка при записи в файл дешифрованных данных */
+class ExceptionUnableToWriteDecryptedTextToFile : public Exceptions {
+public:
+    ExceptionUnableToWriteDecryptedTextToFile() : Exceptions(EXCEPTION_UNABLE_TO_WRITE_DECRYPTEDTEXT_TO_FILE) {}
+
+    const char *what() const noexcept override {
+        return "Unable to write decrypted text to file!";
+    }
+};
+
+/* Исключение: невозможно дописать последний блок шифрования в файл */
+class ExceptionUnableToWriteFinalDataToFile : public Exceptions {
+public:
+    ExceptionUnableToWriteFinalDataToFile() : Exceptions(EXCEPTION_UNABLE_TO_WRITE_FINAL_ENCRYPTED_DATA_TO_FILE) {}
+
+    const char *what() const noexcept override {
+        return "Unable to write final data to file!";
+    }
+};
+
+/* Исключение: невозможно прочитать сигнатуру (соль) из файла для дешифрования*/
+class ExceptionUnableToReadSalt : public Exceptions {
+public:
+    ExceptionUnableToReadSalt() : Exceptions(EXCEPTION_UNABLE_TO_READ_SALT_FROM_FILE) {}
+
+    const char *what() const noexcept override {
+        return "Unable to read salt from file!";
+    }
+};
+
 /*
  *   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =
  *  =   =   =   =  Исключения от OpenSSL функций  =   =   =   =
  *  =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =
 */
+
+
 /* Исключение: ошибка в функции PKCS5_PBKDF2_HMAC() библиотеки OpenSSL */
 class ExceptionOpensslHMAC : public Exceptions {
 public:
@@ -129,6 +170,16 @@ public:
     }
 };
 
+/* Исключение: ошибка функции EVP_EncryptInit_ex()  библиотеки OpenSSL */
+class ExceptionOpensslDecryptInit : public Exceptions {
+public:
+    ExceptionOpensslDecryptInit() : Exceptions(EXCEPTION_OPENSSL_EVP_DecryptInit_ex) {}
+
+    const char *what() const noexcept override {
+        return "Error: EVP_DecryptInit_ex() completed with problem!";
+    }
+};
+
 /* Исключение: ошибка функции EVP_EncryptUpdate()  библиотеки OpenSSL */
 class ExceptionOpensslEncryptUpdate : public Exceptions {
 public:
@@ -139,6 +190,16 @@ public:
     }
 };
 
+/* Исключение: ошибка функции EVP_EncryptUpdate()  библиотеки OpenSSL */
+class ExceptionOpensslDecryptUpdate : public Exceptions {
+public:
+    ExceptionOpensslDecryptUpdate() : Exceptions(EXCEPTION_OPENSSL_EVP_DecryptUpdate) {}
+
+    const char *what() const noexcept override {
+        return "Error: EVP_DecryptUpdate() completed with problem!";
+    }
+};
+
 /* Исключение: ошибка функции EVP_EncryptFinal_ex()  библиотеки OpenSSL */
 class ExceptionOpensslEncryptFinal : public Exceptions {
 public:
@@ -146,6 +207,36 @@ public:
 
     const char *what() const noexcept override {
         return "Error: EVP_EncryptFinal_ex() compelted with problem!";
+    }
+};
+
+/* Исключение: ошибка функции EVP_EncryptFinal_ex()  библиотеки OpenSSL */
+class ExceptionOpensslDecryptFinal : public Exceptions {
+public:
+    ExceptionOpensslDecryptFinal() : Exceptions(EXCEPTION_OPENSSL_EVP_DecryptFinal_ex) {}
+
+    const char *what() const noexcept override {
+        return "Error: EVP_DecryptFinal_ex() compelted with problem!";
+    }
+};
+
+/* Исключение: ошибка функции RAND_bytes()  библиотеки OpenSSL */
+class ExceptionOpensslRandbytes : public Exceptions {
+public:
+    ExceptionOpensslRandbytes() : Exceptions(EXCEPTION_OPENSSL_RAND_bytes) {}
+
+    const char *what() const noexcept override {
+        return "Error: RAND_bytes() completed with problem!";
+    }
+};
+
+/* Исключение: файл может быть повреждён, файл слишком мал, для наличия сигнатуры (соли) для дешфирования */
+class ExceptionFileTooSmallToDecrypt : public Exceptions {
+public:
+    ExceptionFileTooSmallToDecrypt() : Exceptions(EXCEPTION_INVALID_FILE_TO_DECRYPT) {}
+
+    const char *what() const noexcept override {
+        return "File might be corrupted: too small to be encrypted!";
     }
 };
 
